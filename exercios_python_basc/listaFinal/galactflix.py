bc5eaa019dbf7a01849f956d9f095e02a11ac1f2
CLedsonB <74743220+CLedsonB@users.noreply.github.com>
2024-08-22 20:58:43 -0300

    Update galactflix.py

--- exercios_python_basc/listaFinal/galactflix.py
+++ exercios_python_basc/listaFinal/galactflix.py
@@ -5,6 +5,6 @@ lucro = r - d
 margem = (lucro / r) * 100
 roi = (lucro / d) * 100
 
-print(f'Lucro Liquido: {lucro:,.2f}')
-print(f'Margem de Lucro: {margem:,.2f}%')
-print(f'ROI:{ roi:,.2f}%')
+print(f'Lucro Liquido: {lucro:.2f}')
+print(f'Margem de Lucro: {margem:.2f}%')
+print(f'ROI:{ roi:.2f}%')
