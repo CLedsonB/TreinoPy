f7f0f3f326566cd97798b24566c20967adb05efb
CLedsonB <74743220+CLedsonB@users.noreply.github.com>
2024-08-27 18:56:59 -0300

    Update avatar.py

--- exercios_python_basc/listaFinal/avatar.py
+++ exercios_python_basc/listaFinal/avatar.py
@@ -1,4 +1,25 @@
 num = int(input())
 hab = [0]*num
 id = [0]*num
+aux = []
+ids = ""
+cont = 0
 
+aux.extend(input().split())
+hab = list(map(int,aux))
+
+aux = []
+aux.extend(input().split())
+id = list(map(int,aux))
+
+y = int(input())
+
+for i in range(num):
+    if y == hab[i]:
+        ids += str(id[i]) + " "
+        cont += 1
+
+if cont == 0:
+    print("Nenhum")
+else:
+    print(ids)
